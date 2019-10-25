be8bb5e331162aed196415ec1a40901a08577761
Karina Gomes <kariinagomes_@hotmail.com>
2019-10-25 11:06:48 -0300

    create spring boot project

---
+++ src/main/kotlin/com/sciensa/corujaoapi/CorujaoapiApplication.kt
@@ -0,0 +1,11 @@
+package com.sciensa.corujaoapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CorujaoapiApplication
+
+fun main(args: Array<String>) {
+	runApplication<CorujaoapiApplication>(*args)
+}
---
+++ src/test/kotlin/com/sciensa/corujaoapi/CorujaoapiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sciensa.corujaoapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CorujaoapiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
